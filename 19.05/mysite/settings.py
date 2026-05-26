73b63ec6cff5d0fe80219cec8eeb853eab6a74e7
whiteside <kisuriteiro@gmail.com>
2026-05-26 19:24:03 +0300

    Add files via upload

---
+++ 19.05/mysite/settings.py
@@ -0,0 +1,23 @@
+INSTALLED_APPS = [
+    ...,
+    'main',
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / "templates"],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
